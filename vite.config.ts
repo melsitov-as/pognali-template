32190d8aa519a1f70727ba82ec609de74b210ef9
Alexander <gamer34545@mail.ru>
2025-08-09 14:55:34 +0300

    edit urls

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,36 @@
-import { defineConfig } from 'vite';
-import { format, resolve } from 'path';
+import { defineConfig, loadEnv } from 'vite';
 import pugPlugin from 'vite-plugin-pug-transformer';
-import vitePugPlugin from 'vite-plugin-pug';
+import { format, resolve } from 'path';
 
-const FRONT_PATH = 'src';
+export default defineConfig(({ mode }) => {
+  const FRONT_PATH = 'src';
+  const env = loadEnv(mode, process.cwd(), '');
+  const prefix = env.VITE_BASE_URL || '/'; // Используем VITE_BASE_URL или корневой путь по умолчанию
 
-export default defineConfig({
-  root: 'src',
-  build: {
-    rollupOptions: {
-      input: {
-        index: resolve(__dirname, `${FRONT_PATH}/index.html`),
-        catalog: resolve(__dirname, `${FRONT_PATH}/catalog.html`),
-        routes: resolve(__dirname, `${FRONT_PATH}/routes.html`),
+  return {
+    root: 'src',
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, `${FRONT_PATH}/index.html`),
+          catalog: resolve(__dirname, `${FRONT_PATH}/catalog.html`),
+          routes: resolve(__dirname, `${FRONT_PATH}/routes.html`),
+        },
       },
     },
-  },
-  server: {
-    open: 'index.html',
-  },
-  // plugins: [
-  //   pugPlugin({
-  //     pugOptions: {
-  //       pretty: true,
-  //     },
-  //   }),
-  //   vitePugPlugin(),
-  // ],
+    server: {
+      open: 'index.html',
+    },
+    plugins: [
+      pugPlugin({
+        pugOptions: {
+          pretty: true, // Для форматирования HTML-кода
+          locals: {
+            prefix: prefix,
+          },
+        },
+      }),
+    ],
+    base: '/pognali-template/',
+  };
 });
